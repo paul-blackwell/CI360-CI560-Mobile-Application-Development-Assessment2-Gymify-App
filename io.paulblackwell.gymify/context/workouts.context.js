8bede2500170afc3e42dc5631c45e233bce172e2
Paul Blackwell <blackwellpaul34@gmail.com>
2021-02-16 09:29:24 +0000

    Working on storing data locally

--- io.paulblackwell.gymify/context/workouts.context.js
+++ io.paulblackwell.gymify/context/workouts.context.js
@@ -1,15 +1,49 @@
-import React, {useReducer, createContext } from 'react';
+import React, { useReducer, createContext } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
 import workoutsReducer from '../reducers/workouts.reducer';
 import defaultWorkouts from '../data/defaultWorkouts';
 
 
+
+const storeData = async (key, data) => {
+    try {
+        await AsyncStorage.setItem(key, data);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+storeData('workouts', JSON.stringify(defaultWorkouts));
+
+
+
+const retrieveData = async () => {
+    try {
+        const value = await AsyncStorage.getItem('workouts');
+        if (value !== null) {
+            console.log(value);
+        } else {
+            // Save default workouts locally 
+            //storeData('workouts', defaultWorkouts)
+            console.log('there was no data')
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+
+
 export const WorkoutsContext = createContext();
+
+retrieveData();
 
 export const WorkoutsProvider = (props) => {
     const [workouts, dispatch] = useReducer(workoutsReducer, defaultWorkouts);
-    
+
     return (
-        <WorkoutsContext.Provider value={{workouts, dispatch}}>
+        <WorkoutsContext.Provider value={{ workouts, dispatch }}>
             {props.children}
         </WorkoutsContext.Provider>
     );
