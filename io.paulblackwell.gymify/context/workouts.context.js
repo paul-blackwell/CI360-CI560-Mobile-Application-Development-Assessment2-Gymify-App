380342f3b7369053d2794dca954b5ebbd68cf62f
Paul Blackwell <blackwellpaul34@gmail.com>
2021-02-16 10:43:36 +0000

    RetrieveData now returns in the workout.context

--- io.paulblackwell.gymify/context/workouts.context.js
+++ io.paulblackwell.gymify/context/workouts.context.js
@@ -9,23 +9,13 @@ import { storeData, retrieveData } from '../requests/accessLocalStorage';
 
 export const WorkoutsContext = createContext();
 
-const test = retrieveData('workouts');
-
-
-// if(test === null){
-//     console.log('knowing is saved')
-// } else {
-//     console.log(test)
-// }
-
-
 
 export const WorkoutsProvider = (props) => {
 
     useEffect(() => {
         const storage = async () => {
-            const test = retrieveData('workouts');
-            console.log(test)
+            let items = await retrieveData('workouts');
+            console.log(items)
         }
         storage()
     }, []);
