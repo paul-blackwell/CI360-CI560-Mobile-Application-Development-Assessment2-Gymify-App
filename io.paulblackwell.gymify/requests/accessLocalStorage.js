e608cc9b1b4c15f94be4625f00fd53c1428b62c1
Paul Blackwell <blackwellpaul34@gmail.com>
2021-02-16 10:13:00 +0000

    Add comment and made changes to storeData and retriveData

--- io.paulblackwell.gymify/requests/accessLocalStorage.js
+++ io.paulblackwell.gymify/requests/accessLocalStorage.js
@@ -1,23 +1,37 @@
 import AsyncStorage from '@react-native-community/async-storage';
+import defaultWorkouts from '../data/defaultWorkouts';
 
+/**
+ * This function will save data to local storage
+ * @param {string} key - This key will be used as a keyExtractor so the data can be retrieved later.
+ * @param {object} data -This will be the data that needs to be saved but will be converted into a string.
+ */
 export const storeData = async (key, data) => {
     try {
-        await AsyncStorage.setItem(key, data);
+        await AsyncStorage.setItem(key, JSON.stringify(data));
     } catch (error) {
         console.log(error);
     }
 };
 
 
-export const retrieveData = async () => {
+/**
+ * This function will get any saved data from local storage based on a key.
+ * However, if no data is returned, it will save the default workouts to local 
+ * storage and return that data.
+ * @param {string} key - This is the key the data is stored under.
+ * @return {object} - The data stored 
+ */
+export const retrieveData = async (key) => {
     try {
-        const value = await AsyncStorage.getItem('workouts');
+        const value = await AsyncStorage.getItem(key);
         if (value !== null) {
-            console.log(value);
+            return JSON.parse(value);
         } else {
             // Save default workouts locally 
-            //storeData('workouts', defaultWorkouts)
-            console.log('there was no data')
+            storeData('workouts', defaultWorkouts);
+            // The function will call itself (again), now the default workouts have been saved.
+            retrieveData();
         }
     } catch (error) {
         console.log(error);
