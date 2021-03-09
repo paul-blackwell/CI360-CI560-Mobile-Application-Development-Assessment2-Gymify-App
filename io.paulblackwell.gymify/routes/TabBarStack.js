96c5a1824c14bab6daae33c848fbf692da8a51e6
Paul Blackwell <blackwellpaul34@gmail.com>
2021-03-09 10:46:48 +0000

    Started work on the tab navigation styles

--- io.paulblackwell.gymify/routes/TabBarStack.js
+++ io.paulblackwell.gymify/routes/TabBarStack.js
@@ -2,19 +2,35 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-// import HomeStack from '../routes/HomeStack';
 import WorkoutStack from '../routes/WorkoutStack';
 import HomeScreen from '../screens/HomeScreen';
 import ProgressScreen from '../screens/ProgressScreen';
+import colors from '../styles/colors'
 
 
 const Tab = createBottomTabNavigator();
 
 export default function TabBarStack() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      tabBarOptions={
+        {
+          activeTintColor: colors.purple[200],
+          inactiveTintColor: colors.gray[300],
+          style: {
+            paddingBottom: 8,
+          },
+        }
+      }
+    >
       <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="WorkoutStack" component={WorkoutStack} />
+      <Tab.Screen
+        name="WorkoutStack"
+        component={WorkoutStack}
+        options={{
+          tabBarLabel: 'Workouts'
+        }}
+      />
       <Tab.Screen name="Progress" component={ProgressScreen} />
     </Tab.Navigator>
   );
