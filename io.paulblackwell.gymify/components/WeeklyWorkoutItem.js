4c5778b3939d6dc45fb8ca222c0abfc4d9eae1f0
Paul Blackwell <blackwellpaul34@gmail.com>
2021-03-11 10:14:45 +0000

    was working on adding styles to WorkoutsListItem component

--- io.paulblackwell.gymify/components/WeeklyWorkoutItem.js
+++ io.paulblackwell.gymify/components/WeeklyWorkoutItem.js
@@ -1,34 +1,50 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 import colors from '../styles/colors'
 
+
 export default WeeklyWorkoutItem = ({ title, id, navigation }) => (
-    <View style={styles.item}>
-        <Text>{title}</Text>
-        <Button
-            title="Go to workout"
-            onPress={() => {
-                //Go to WorkoutList screen and pass the workout id to that screen
-                navigation.navigate('WorkoutStack', {
-                    screen: 'WorkoutsScreen',
-                    params: { weekId: id }
-                });
-            }}
-        />
-    </View>
+    <TouchableOpacity 
+    style={styles.item} 
+    onPress={() => {
+         //Go to WorkoutList screen and pass the workout id to that screen
+         navigation.navigate('WorkoutStack', {
+            screen: 'WorkoutsScreen',
+            params: { weekId: id }
+        });
+    }}>
+        <View style={styles.itemTitle}>
+            <Text style={styles.itemTitleText}>{title}</Text>
+        </View>
+        <View style={styles.itemIconContainer}>
+            <AntDesign name="arrowright" size={24} color={colors.gray[300]} />
+        </View>
+    </TouchableOpacity>
 );
 
 
 const styles = StyleSheet.create({
     item: {
-        // width: '100%',
+        flexDirection: 'row',
         backgroundColor: colors.white[100],
         padding: 20,
         marginVertical: 8,
-        // marginHorizontal: 16,
         borderColor: colors.gray[200],
         borderWidth: 1,
         borderRadius: 4,
+    },
+    itemTitle: {
+        justifyContent: 'center',
+        width: '50%',
+    },
+    itemTitleText: {
+        fontSize: 18,
+        color: colors.gray[300]
+    },
+    itemIconContainer: {
+        width: '50%',
+        alignItems: 'flex-end',
+        justifyContent: 'center',
     },
 });
