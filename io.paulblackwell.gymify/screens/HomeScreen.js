ad24500cab0d5d583093e83fff778c6658ff6e8d
Paul Blackwell <blackwellpaul34@gmail.com>
2021-03-11 16:11:50 +0000

    Deleted the code/comment  that wasn't being used

--- io.paulblackwell.gymify/screens/HomeScreen.js
+++ io.paulblackwell.gymify/screens/HomeScreen.js
@@ -14,56 +14,7 @@ export default function HomeScreen({ navigation }) {
   // Get workouts context with will be an array with all of the workouts
   const { workoutPlan } = useContext(WorkoutsContext);
 
-  /**
-   * Check if WorkoutPlan has been saved locally if it hasn't save it locally.
-   * This needs to be done as we can't save this data locally in the API request made in
-   * the workouts content, so we have to do it here. The API request in the workouts content
-   * will only run one time as its only ever made when the app is loaded for the first time, 
-   * so this code will only run one time.
-   */
-
-  /* ------------------------------------------------------------------------------------------- */
-
-  // const [workoutPlanIsSavedLocally, setWorkoutPlanIsSavedLocally] = useState(null)
-  // const fetchFromLocalStorage = async () => {
-  //   console.log('I was fired')
-  //   try {
-  //     const value = await AsyncStorage.getItem('workoutPlan');
-  //     if (value === null) {
-  //       console.log('value was null')
-  //       console.log(value)
-  //       setWorkoutPlanIsSavedLocally(false)
-  //     } else if (value !== null) {
-  //       console.log('Data has been saved')
-  //     }
-  //   } catch (e) {
-  //     console.log(e)
-  //   }
-  // }
-
-  // const pushToLocalStorage = async () => {
-  //   try {
-  //     console.log('I was fired as well')
-  //     await AsyncStorage.setItem('workoutPlan', JSON.stringify(workoutPlan));
-  //     setWorkoutPlanIsSavedLocally(true)
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   if(workoutPlanIsSavedLocally === null) {
-  //     fetchFromLocalStorage();
-  //   } else if (workoutPlanIsSavedLocally === false) {
-  //     pushToLocalStorage();
-  //   }
-  // }, [workoutPlanIsSavedLocally]);
-
-  // console.log(workoutPlan.post)
-
-  /* ------------------------------------------------------------------------------------------- */
   
-
   const renderItem = ({ item }) => (
     <WeeklyWorkoutItem title={item.title} id={item.id} navigation={navigation} />
   );
