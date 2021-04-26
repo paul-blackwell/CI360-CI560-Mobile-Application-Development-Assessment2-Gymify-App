09063251149aafbb6f4621c94b41819a87605cd9
Paul Blackwell <blackwellpaul34@gmail.com>
2021-04-26 08:59:43 +0100

    Started work on just using an API instead of local storage

--- io.paulblackwell.gymify/context/workouts.context.js
+++ io.paulblackwell.gymify/context/workouts.context.js
@@ -22,55 +22,26 @@ export const WorkoutsProvider = (props) => {
     const [workoutPlan, dispatch] = useReducer(reducer, initialState);
 
 
-    // Just for testing
-    //deleteLocalData('workoutPlan')
-
-
-    // This will be used to save data from API locally
-    // const pushToLocalStorage = async (data) => {
-    //     try {
-    //         await AsyncStorage.setItem('workoutPlan', JSON.stringify(data));
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // }
-
-
 
     useEffect(() => {
 
-        /**
-         * This will get the workout plan from AsyncStorage (local data on the phone) 
-         * and set the context to the data returned from it however, if the workout plan 
-         * is not currently saved in local storage then an API request is made to get the
-         *  workout data from an external source. After this data is sourced externally it 
-         * is set to the context and then save the data to local storage so next time the user 
-         * opens the device the data will be saved locally.
-         */
         const request = async () => {
-
-            const value = await AsyncStorage.getItem('workoutPlan');
-            if (value !== null) {
-                dispatch({ type: 'FETCH_LOCAL_STORAGE_SUCCESS', payload: JSON.parse(value) })
-            } else if (value === null) {
-                axios
-                    .get('https://cryptic-garden-88403.herokuapp.com/workout-plans')
-                    .then(response => {
-                        // Pass response data to dispatch in reducer
-                        dispatch({ type: 'FETCH_API_SUCCESS', payload: response.data })
-                        // Save data locally 
-                        //pushToLocalStorage(response.data)
-                    })
-                    .catch(error => {
-                        dispatch({ type: 'FETCH_API_ERROR' })
-                        console.log(error)
-                    });
-            }
-        }
+            axios
+                .get('https://cryptic-garden-88403.herokuapp.com/workout-plans')
+                .then(response => {
+                    // Pass response data to dispatch in reducer
+                    dispatch({ type: 'FETCH_API_SUCCESS', payload: response.data })
+                })
+                .catch(error => {
+                    dispatch({ type: 'FETCH_API_ERROR' })
+                    console.log(error)
+                });
+    }
 
         request();
 
     }, []);
+
 
 
 
--- io.paulblackwell.gymify/screens/HomeScreen.js
+++ io.paulblackwell.gymify/screens/HomeScreen.js
@@ -18,20 +18,22 @@ export default function HomeScreen({ navigation }) {
   // Get workouts context with will be an array with all of the workouts
   const { workoutPlan } = useContext(WorkoutsContext);
 
+ // console.log(workoutPlan);
+
   
   /**
    * Save update local storage to the current context, nothing 
    * will change if the current context has already come from local storage
    */
   //dispatch({ type: 'UPDATE_LOCAL_STORAGE', payload: workoutPlan })
-  useEffect(() => {
-    pushToLocalStorage('workoutPlan', workoutPlan)
-  }, [])
+  // useEffect(() => {
+  //   pushToLocalStorage('workoutPlan', workoutPlan)
+  // }, [])
 
   //console.log(workoutPlan)
 
   // Just for testing
-  showLocalDataKeys()
+  //showLocalDataKeys()
   //deleteLocalData('workoutPlan')
 
 
