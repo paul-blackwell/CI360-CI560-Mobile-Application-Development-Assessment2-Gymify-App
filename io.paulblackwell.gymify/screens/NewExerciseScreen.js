d7bd1be45acf185f760fb12f0e0001c418dfd0f3
Paul Blackwell <blackwellpaul34@gmail.com>
2021-05-12 08:05:15 +0100

    Started work on the NewExerciseScreen inputs

--- io.paulblackwell.gymify/screens/NewExerciseScreen.js
+++ io.paulblackwell.gymify/screens/NewExerciseScreen.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet, Text, View, StatusBar, SafeAreaView } from 'react-native';
+import React, { useState, useEffect, useContext } from 'react';
+import { StyleSheet, Text, View, StatusBar, SafeAreaView, TextInput } from 'react-native';
 import NewExerciseBtnPrimary from '../components/smallerComponents/NewExerciseBtnPrimary';
 
 import { standardColors } from '../styles/colors';
@@ -7,12 +7,37 @@ let colors = standardColors;
 
 export default function NewExerciseScreen({ navigation }) {
 
+  const [exerciseName, setExerciseName] = useState('');
+  const [addTimer, setAddTimer] = useState(false);
+  const [addTime, setAddTime] = useState(0);
+  const [numberOfSets, setNumberOfSets] = useState(0);
+  const [numberOfReps, setNumberOfReps] = useState(0);
+  const [description, setDescription] = useState('');
+
+  //const [addTimer, setAddTimer] = useState(false);
+
+  // const [formState, setFormState] = useState({
+  //   exerciseName: '',
+  //   numberOfSets: 0,
+  //   numberOfReps: 0,
+  //   time: 0,
+  //   description: ''
+  // });
+
   return (
     <>
       <SafeAreaView style={styles.container}>
         <StatusBar barStyle="light-content" backgroundColor={colors.purple[200]} />
-        <View style={styles.main}>
-          <Text >New Exercise Screen</Text>
+        <View style={styles.inputSection}>
+          <Text style={styles.inputLabel}>Exercise name</Text>
+          <View style={{ height: 32 }}>
+            <TextInput
+              style={styles.input}
+              onChangeText={setExerciseName}
+              value={exerciseName}
+              placeholder=""
+            />
+          </View>
         </View>
       </SafeAreaView>
       <View style={styles.newExerciseTabBar}>
@@ -29,12 +54,6 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     backgroundColor: colors.gray[100]
   },
-  main: {
-    flex: 1,
-    justifyContent: 'center',
-    margin: 10,
-    alignItems: 'center'
-  },
   newExerciseTabBar: {
     position: 'absolute',
     justifyContent: 'center',
@@ -48,4 +67,23 @@ const styles = StyleSheet.create({
     elevation: 3,
     zIndex: 1000
   },
+  inputSection: {
+    marginTop: 16
+    //backgroundColor: colors.purple[100]
+  },
+  inputContainer: {
+    height: 32
+  },
+  inputLabel: {
+    color: colors.gray[300],
+    marginBottom: 8
+  },
+  input: {
+    flex: 1,
+    padding: 4,
+    backgroundColor: colors.white[100],
+    borderColor: colors.gray[200],
+    borderWidth: 1,
+    borderRadius: 4,
+  }
 });
